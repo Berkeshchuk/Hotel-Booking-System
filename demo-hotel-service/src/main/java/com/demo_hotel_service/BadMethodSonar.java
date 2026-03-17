258c44acdfa2b8a8b3eae97072f885c746a3be4c
Олександр Беркещук <sanya.bero@gmail.com>
2026-03-17 11:35:56 +0200

    any message

--- demo-hotel-service/src/main/java/com/demo_hotel_service/BadMethodSonar.java
+++ demo-hotel-service/src/main/java/com/demo_hotel_service/BadMethodSonar.java
@@ -1,13 +1,8 @@
 package com.demo_hotel_service;
 
 public class BadMethodSonar {
-    void badMethod() {
-        Object object = null;
-        object.toString();
-    }
-
-    public void myBadMethod() {
-    String dbPassword = "super_secret_password_123"; // SonarQube ненавидить захардкоджені паролі
-    System.out.println(dbPassword);
-}
+    // void badMethod() {
+    //     Object object = null;
+    //     object.toString();
+    // }
 }
